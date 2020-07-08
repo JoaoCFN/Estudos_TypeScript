f5a30367b0a65572760de5835f279a0c48178f1f
João Neto <joaoneto.20fsa@gmail.com>
2020-07-08 15:24:31 -0300

    Union e Type Alias

---
+++ logDetails.ts
@@ -0,0 +1,39 @@
+/*  
+    O union "|" serve para dizer a uma var que ela pode ser de outros tipos.
+    No exemplo abaixo temos o "uid" podendo ser number e string 
+*/
+
+/* 
+    O type alias permite que sejam criados tipos personalizados e ajude a gente a reaproveitar código. No exemplo abaixo temos o Uid virando um tipo para o parâmetro uid das duas funções.
+*/
+
+/* -------------------------------------------------------------- */
+type Uid = number | string
+
+function logDetails(uid: Uid, item: string){
+    console.log(`A product with ${uid} has a title as ${item}`);
+}
+
+logDetails(123, "sapato");
+logDetails("123", "sapato");
+
+function logInfo(uid: Uid, user: string){
+    console.log(`An user with ${uid} has a name as ${user}`);
+}
+
+logInfo(123, "João");
+logInfo("123", "João");
+/* -------------------------------------------------------------- */
+
+/* 
+    Podemos usar também o type para predefinir os tipos para serem usados na aplicação como no exemplo abaixo 
+*/
+
+type Plataform = "Windows" | "Linux" | "Mac Os";
+
+function renderPlataform(plataform: Plataform){
+    return plataform;
+}
+
+// renderPlataform("ios"); Não roda pois ios não é uma plataforma definida
+renderPlataform("Windows"); // roda pois windows é uma plataforma definida
--- types.ts
+++ types.ts
@@ -1,24 +1,24 @@
 // boolean
-let isOpen: boolean
-isOpen = true
+let isOpen: boolean;
+isOpen = true;
 
 // string
-let message: string
-message = "Hello World"
+let message: string;
+message = "Hello World";
 
 // number
-let total: number
-total = 20
+let total: number;
+total = 20;
 
 // array
-let items: number[]
-items = [1, 2, 3]
-let moreItems:  Array<number>
-moreItems = [1, 2, 3]
+let items: number[];
+items = [1, 2, 3];
+let moreItems:  Array<number>;
+moreItems = [1, 2, 3];
 
 // tuple
-let title: [number, string]
-title = [1, "Vamos lá"]
+let title: [number, string];
+title = [1, "Vamos lá"];
 
 // enum
 enum Colors{
@@ -27,16 +27,16 @@ enum Colors{
 }
 
 // any (qualquer coisa)
-let coisa: any
-coisa = []
+let coisa: any;
+coisa = [];
 
 // void
 function logger(){
-    console.log("curso de typescript")
+    console.log("curso de typescript");
 }
 
 // null || undefined
-type Bla = string | undefined
+type Bla = string | undefined;
 
 // never
 function error(): never {
@@ -47,4 +47,8 @@ function error(): never {
 let cart: object
 cart = {
     key: "car"
-}
+};
+
+// Type Inference
+// Em alguns casos o typescript já começa a entender sozinho os tipos de variáveis. Isso ajuda a não ficar tipando de forma redundante.
+let message2 = "mensagem definida";
